7a8ef4af9b34402ffb18ef35c483a56516ad8c43
周顺顺 <idioticzhou@foxmail.com>
2022-07-07 14:57:20 +0800

    feat(MenuSetting): 🌟 补充菜单功能和接口调用

--- src/views/admin/menu/index.vue
+++ src/views/admin/menu/index.vue
@@ -104,13 +104,13 @@
             :label-width="100"
             class="py-4"
           >
-            <NFormItem
+            <n-form-item
               :label="t('global.type')"
               path="type"
             >
               <span>{{ formParams.type === 1 ? t('global.siderMenu') : '' }}</span>
-            </NFormItem>
-            <NFormItem
+            </n-form-item>
+            <n-form-item
               :label="t('admin.global.menuName')"
               path="label"
             >
@@ -118,8 +118,8 @@
                 v-model:value="formParams.label"
                 :placeholder="t('global.inputPlaceholder',[t('admin.global.menuName')])"
               />
-            </NFormItem>
-            <NFormItem
+            </n-form-item>
+            <n-form-item
               :label="t('admin.global.localeKey')"
               path="subtitle"
             >
@@ -127,8 +127,8 @@
                 v-model:value="formParams.subtitle"
                 :placeholder="t('global.inputPlaceholder',[t('admin.global.localeKey')])"
               />
-            </NFormItem>
-            <NFormItem
+            </n-form-item>
+            <n-form-item
               :label="t('global.path')"
               path="key"
             >
@@ -136,8 +136,8 @@
                 v-model:value="formParams.key"
                 :placeholder="t('global.inputPlaceholder',[t('global.path')])"
               />
-            </NFormItem>
-            <NFormItem
+            </n-form-item>
+            <n-form-item
               :label="t('admin.global.openType')"
               path="openType"
             >
@@ -154,17 +154,23 @@
                   </NRadio>
                 </NSpace>
               </NRadioGroup>
-            </NFormItem>
-            <NFormItem
-              :label="t('admin.global.menuAuth')"
+            </n-form-item>
+            <n-form-item
+              v-show="curMenuFuncs.length > 0"
+              :label="t('admin.global.menubar')"
               path="auth"
             >
-              <NInput
-                v-model:value="formParams.auth"
-                :placeholder="t('admin.global.authPlaceholder')"
-              />
-            </NFormItem>
-            <NFormItem
+              <n-checkbox-group />
+              <n-space>
+                <n-checkbox
+                  v-for="(item) in curMenuFuncs"
+                  :key="item.funccode"
+                  :value="item.funccode"
+                  :label="item.memo"
+                />
+              </n-space>
+            </n-form-item>
+            <n-form-item
               path="auth"
               style="margin-left: 100px"
             >
@@ -183,7 +189,7 @@
                   {{ t('global.delete') }}
                 </NButton>
               </NSpace>
-            </NFormItem>
+            </n-form-item>
           </NForm>
         </NCard>
       </NGi>
@@ -202,6 +208,8 @@ import {
 } from '@vicons/antd';
 import { useI18n } from 'vue-i18n';
 import { useAsyncRouteStore } from '@/store';
+import systemApi from '@/api/system';
+import { MenuFunc } from '#/api';
 
 const { t } = useI18n();
 
@@ -224,13 +232,16 @@ const formRef = ref<FormInst>();
 const message = useMessage();
 const dialog = useDialog();
 
-const expandedKeys = ref([]);
+const menuFuncs = new Map();
 
+const expandedKeys = ref([]);
 const loading = ref(true);
 const subLoading = ref(false);
 const isEditMenu = ref(false);
 const treeItemTitle = ref('');
 const pattern = ref('');
+
+const curMenuFuncs = ref< MenuFunc[]>([]);
 
 const formParams = reactive({
   type: 1,
@@ -246,8 +257,25 @@ const treeData = computed(() => rs.menus.map((item) => ({
   ...item,
 })));
 
-function selectedTree(keys) {
+async function getMenuFuncsByMenuId(menuId: number) {
+  const res = await systemApi.getMenuFuncsByMenuId(menuId);
+  if (res.code === 1) {
+    return res.data;
+  }
+  message.error(res.msg);
+  return [];
+}
+
+async function selectedTree(keys) {
   const selectData = treeData.value.filter((item) => item.key === keys[0])[0];
+  if (!menuFuncs.has(selectData.id)) {
+    const res = await getMenuFuncsByMenuId(selectData.id);
+    menuFuncs.set(selectData.id, res);
+    curMenuFuncs.value = res;
+  } else {
+    curMenuFuncs.value = menuFuncs.get(selectData.id);
+  }
+
   treeItemTitle.value = selectData.label;
   Object.assign(formParams, selectData);
   isEditMenu.value = true;
