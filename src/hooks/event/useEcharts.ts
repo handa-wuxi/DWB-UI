0bc39926bc6771fa7e5da211c62029ef9c3e2dcf
周顺顺 <idioticzhou@foxmail.com>
2022-07-09 15:43:47 +0800

    feat(echart): 🌟 安装echart组件，并编写相关hook，提升开发效率

---
+++ src/hooks/event/useEcharts.ts
@@ -0,0 +1,46 @@
+// 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
+import * as echarts from 'echarts/core';
+import { LineChart } from 'echarts/charts';
+import {
+  GridComponent, TitleComponent, TooltipComponent, LegendComponent, ToolboxComponent,
+} from 'echarts/components';
+import { LabelLayout, UniversalTransition } from 'echarts/features';
+import { CanvasRenderer } from 'echarts/renderers';
+import { EChartsOption } from 'echarts';
+import { VueElement, onUnmounted } from 'vue';
+
+// 注册必须的组件
+echarts.use([
+  LabelLayout,
+  GridComponent,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  ToolboxComponent,
+  UniversalTransition,
+  CanvasRenderer,
+]);
+
+export const useEcharts = () => {
+  const useLineChart = (inst:VueElement, data: EChartsOption) => {
+    echarts.use([LineChart]);
+    const myChart = echarts.init(inst);
+    myChart.setOption(data);
+    window.addEventListener('resize', () => {
+      myChart.resize();
+    });
+    onUnmounted(() => {
+      myChart.dispose();
+    });
+  };
+
+  return { useLineChart };
+};
+
+export const useGenMonths = () => {
+  const months: string[] = [];
+  for (let i = 1; i <= 12; i += 1) {
+    months.push(`${i}月`);
+  }
+  return months;
+};
